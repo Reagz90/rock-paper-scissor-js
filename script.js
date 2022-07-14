06f32891f78596d130b5489ead5026f206457050
Reagan Hammond <reagan.hammond@open.edu.au>
2022-07-14 21:11:20 +1000

    added for loop for 5 round game that records results, all via console as no UI implemented yet

--- script.js
+++ script.js
@@ -3,25 +3,58 @@
    Play against the computer.
    May the best player win!
 *****************************/
-const playerSelection = prompt("Pick your play: Rock, Paper or Scissors?");
-const computerSelection = computerPlay();
-console.log(`Player selection = ${playerSelection}`);
-console.log(`Computer selection = ${computerSelection}`);
-alert(playRound(playerSelection, computerSelection));
 
+game();
+
+//best of 5 rounds
 function game() {
-  let playerScore;
-  let computerScore;
+  let playerScore = 0;
+  let computerScore = 0;
+  let draws = 0;
+
+  for (i = 1; i <= 5; i++) {
+    const playerSelection = prompt("Pick your play: Rock, Paper or Scissors?");
+    const computerSelection = computerPlay();
+
+    console.log(`*****ROUND ${i}*****`);
+    console.log(`Player selection = ${playerSelection}`);
+    console.log(`Computer selection = ${computerSelection}`);
+
+    let roundResult = playRound(playerSelection, computerSelection);
+    console.log(roundResult);
+
+    if (roundResult.includes("You Win!")) {
+      playerScore++;
+    } else if (roundResult.includes("You Lose!")) {
+      computerScore++;
+    } else {
+      draws++;
+    }
+    console.log(`Player Score = ${playerScore}`);
+    console.log(`Computer Score = ${computerScore}`);
+    console.log(`Draws = ${draws}`);
+  }
+
+  if (playerScore > computerScore) {
+    console.log(`You win best of 5 rounds!\
+    You won ${playerScore} rounds!`);
+  } else if (computerScore > playerScore) {
+    console.log(`You lost best of 5 rounds!\
+    Computer won ${computerScore} rounds!`);
+  } else {
+    console.log("Best of 5 was a draw!");
+  }
 }
 
+//rock paper scissors core game function
 function playRound(playerSelection, computerSelection) {
   playerSelection =
     playerSelection.charAt(0).toUpperCase() +
-    playerSelection.slice(1).toLowerCase();
+    playerSelection.slice(1).toLowerCase().trim();
   let playerLoses = `You Lose! ${computerSelection} beats ${playerSelection}!`;
   let draw = "Draw, play again!";
-  let playerWins = `You win, ${playerSelection} beats ${computerSelection}`;
-  let invalidInput = "Please enter Rock, Paper or Scissors!"; 
+  let playerWins = `You Win! ${playerSelection} beats ${computerSelection}`;
+  let invalidInput = "Please enter Rock, Paper or Scissors!";
 
   if (
     (playerSelection == "Rock" && computerSelection == "Paper") ||
@@ -31,13 +64,18 @@ function playRound(playerSelection, computerSelection) {
     return playerLoses;
   } else if (playerSelection == computerSelection) {
     return draw;
-  } else if (playerSelection !== "Rock" || "Paper" || "Scissors") {
-    return invalidInput; 
+  } else if (
+    playerSelection != "Rock" &&
+    playerSelection != "Paper" &&
+    playerSelection != "Scissors"
+  ) {
+    return invalidInput;
   } else {
     return playerWins;
   }
 }
 
+//computer play function
 function computerPlay() {
   var myArray = ["Rock", "Paper", "Scissors"];
 
